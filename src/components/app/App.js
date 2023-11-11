7b06a1fdac626806508205634bd07b891713b67f
Wastiz <valnos04@gmail.com>
2023-11-12 00:57:55 +0200

    another

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import Header from '../header/Header';
 import Navbar from '../navbar/Navbar';
@@ -8,7 +10,22 @@ import {BrowserRouter, Routes, Route,} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from '../../redux/store'
 
-function App() {
+function App () {
+	const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.get('/api/users');
+                setUsers(result.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
   	return (
 		<Provider store={store}>
 			<BrowserRouter>
@@ -21,6 +38,14 @@ function App() {
 							<Route exact path='/teams' element={<Teams/>}/ >
 							<Route exact path='/sign-up' element={<SigninForm/>}/ >
 						</Routes>
+						<div>
+							<h1>List of Users</h1>
+							<ul>
+								{users.map(user => (
+									<li key={user._id}>{user.username} - {user.email}</li>
+								))}
+							</ul>
+						</div>
 					</main>
 				</div>
 			</BrowserRouter>
