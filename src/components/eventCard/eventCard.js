37a3cdd348b2474bffc653109846fb8a207aec28
wastiz <valnos04@gmail.com>
2024-05-30 19:29:55 +0300

    another

--- src/components/eventCard/eventCard.js
+++ src/components/eventCard/eventCard.js
@@ -2,19 +2,21 @@ import './eventCard.scss';
 import placeholder from '../../img/placeholder.png';
 import { Link } from 'react-router-dom'
 
-function EventCard ({ _id, date, location, price, start, title }) {
+function EventCard ({ _id, date, photos: { coverPhoto }, location, price, times: {start}, title, description }) {
     return (
         <div className="card grid grid-cols-2 w-10/12 h-80 bg-base-100 shadow-xl">
-            <div className='col-span-2 bg-primary h-1/2 text-white text-center'>
+            <div className='col-span-2 bg-primary text-white text-center'>
                 {title}
             </div>
             <figure className="m-w-full h-full">
-                <img src={placeholder} alt="event cover" className="rounded-xl"/>
+                <img src={coverPhoto? coverPhoto : placeholder} alt="event cover" className="rounded-xl"/>
             </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">Date: {date}</h2>
-                <h2 className="card-title">Location: {location}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-body">
+                <h2 className="text-white">Date: {date}</h2>
+                <h2 className="text-white">Start time: {start}</h2>
+                <h2 className="text-white">Location: {location}</h2>
+                <h2 className="text-white">Ticket: {price}</h2>
+                <p className='text-white'>{description.substring(0, 40) + "..."}</p>
                 <div className="card-actions">
                     <Link to={`/events/${_id}`}>
                         <button className='btn btn-primary event-btn'>View</button>
